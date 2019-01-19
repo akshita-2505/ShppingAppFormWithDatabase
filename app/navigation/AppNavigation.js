68b76ef895be045e6b4fcbb5b96c65c5f4182d9b
Mac2019_1 <akshitapatel522@gmail.com>
2019-01-19 10:21:35 +0530

    change

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -1,20 +1,18 @@
-import { createStackNavigator, createAppContainer,createDrawerNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Users from '../components/user';
 import UserRegistration from '../components/registration';
 import Login from '../components/login';
 import Signup from '../components/signup';
-import Welcome from '../components/welcome';
 
-const AppNavigator1 = createDrawerNavigator({
-    UserRegistration,
-    Users,
-    Login,
-    Signup
-});
 const AppNavigator = createStackNavigator({
-    Welcome
+    Signup,
+    UserRegistration,
+    Login,
+    Users
+},{
+    initialRoute:Login
 });
 
-const NavigationContainer = createAppContainer(AppNavigator,AppNavigator1);
+const NavigationContainer = createAppContainer(AppNavigator);
 
 export default NavigationContainer;
